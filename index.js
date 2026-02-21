a6fc02129b6c060eee7fe852ab1c3b295761f4ad
Saurabh-Chede <saurabhchede21@gmail.com>
2026-02-21 13:39:29 +0530

    dsa test 2

---
+++ index.js
@@ -0,0 +1,100 @@
+
+// 2️⃣ Check Divisible by Both 3 and 5
+
+// Return true if number is divisible by both.
+
+// Input: 15
+// Output: true
+
+let Input = 15
+
+if(Input%3===0 && Input%5===0){
+    console.log(true)
+}
+else{
+    console.log(false)
+}
+
+
+// 4️⃣ Repeat String N Times
+
+// Repeat a string N times without using repeat().
+
+// Input: "Hi", 3
+// Output: "HiHiHi"
+
+function Repeat (num){
+   let string = 'HI'
+   for(let i=0; i<=num; i++){
+    console.log(string)
+   }
+}
+
+Repeat(10)
+
+// ----------------------------------------------
+
+// 5️⃣ Count Words in a Sentence
+
+// Count number of words in a string.
+
+// Input: "I love MERN stack"
+// Output: 4
+
+let Sentence = 'I love MERN stack'
+let words = 0
+
+
+
+// ----------------------------------------
+
+// 6️⃣ Check if Array is Empty
+
+// Return true if array has no elements.
+
+// Input: []
+// Output: true
+
+let arr = []
+
+function checkEmpty (arr){
+     if(arr.length===0){
+        console.log(true)
+     }else{
+        console.log(false)
+     }
+}
+
+checkEmpty(arr)
+
+
+// 7️⃣ Find Index of Element (Without indexOf)
+
+// Return index of given element manually.
+
+// Input: [5, 8, 2, 9], find 2
+// Output: 2
+
+
+let inputArray = [5, 8, 2, 9]
+
+let target = 9
+
+function ReturnIndex(inputArray,target){
+
+    for(let i=0; i<inputArray.length; i++){
+        if(inputArray[i]===target){
+            return i;
+            
+        }
+        else{
+            console.log('not found')
+        }
+    }
+
+}
+
+console.log(ReturnIndex(inputArray,target))
+
+
+
